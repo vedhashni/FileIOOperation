1fb9f71a9199e6c8c13b964269fc039639b7ead1
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-26 22:24:55 +0530

    [Vedhashni] Updated the file to perform csvoperations

---
+++ FileOperations/CsvOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace FileOperations
+{
+    class CsvOperations
+    {
+        public static string importFilePath = @"C:\Users\ven\source\repos\FileOperations\FileOperations\csvdata.csv";
+        //Read Content of CSV File and Print
+        public static void CsvDeSerailize()
+        {
+            //Initialization
+
+            using var reader = new StreamReader(importFilePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var result = csv.GetRecords<Person>().ToList();
+            foreach (Person m in result)
+            {
+                Console.WriteLine("Name: {0} \t Email: {1} \t Phone Number: {2} \t Country: {3}", m.name, m.email, m.phoneNum, m.country);
+            }
+        }
+
+        public static void CsvSerailize()
+        {
+            string exportfile = @"C:\Users\Radhika\source\repos\FileIOOperations\FileIOOperations\csvdata.csv";
+            using var writer = new StreamWriter(exportfile);
+
+            using var csvWrite = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            List<Person> data = new List<Person>
+            {
+            new Person("Gayatri","gaya@yahoo.com",9894527257,"India") };
+            csvWrite.WriteRecords(data);
+            csvWrite.NextRecordAsync();
+        }
+    }
+}
---
+++ FileOperations/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+    class Person
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public long phoneNum { get; set; }
+        public string country { get; set; }
+
+        public Person(string name, string email, long phoneNum, string country)
+        {
+            this.name = name;
+            this.email = email;
+            this.phoneNum = phoneNum;
+            this.country = country;
+        }
+    }
+}
